430b2557f641d30c1cb70e43d2039c6eeb467fc0
FilipeAbdo <filipeibraim@gmail.com>
2022-02-15 20:39:36 -0300

    Blinking GPIO 1 20 times

--- LedBlink/blink.c
+++ LedBlink/blink.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <wiringPi.h>
 
 int main(void){
+    wiringPiSetup();
+    pinMode(1, OUTPUT);
     printf("Hello World\n");
+    for(int i=0; i<20; i++){
+        printf("i = %i\n", i);
+        digitalWrite(1, HIGH);
+        delay(300);
+        digitalWrite(1, LOW);
+        delay(300);
+    }
     return 0;
 }
