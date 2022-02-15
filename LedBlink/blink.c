2ee8863dbfb4c3b829555067c92c4e4bab497cd9
FilipeAbdo <filipeibraim@gmail.com>
2022-02-15 20:58:59 -0300

    Blinking GPIO 1 3 times when GPIO 4 INPUT == 1

--- LedBlink/blink.c
+++ LedBlink/blink.c
@@ -2,16 +2,34 @@
 #include <stdint.h>
 #include <wiringPi.h>
 
-int main(void){
+#define BLINK_CYCLES 3
+#define DEBOUNCE_x10ms 5
+
+int main(void)
+{
     wiringPiSetup();
     pinMode(1, OUTPUT);
+    pinMode(4, INPUT);
+    pullUpDnControl(4, PUD_UP);
     printf("Hello World\n");
-    for(int i=0; i<20; i++){
-        printf("i = %i\n", i);
-        digitalWrite(1, HIGH);
-        delay(300);
-        digitalWrite(1, LOW);
-        delay(300);
+    for (;;)
+    {
+        int debounce = 0;
+        while (digitalRead(4))
+        {
+            debounce++;
+        }
+        if (debounce >= DEBOUNCE_x10ms)
+        {
+            for (int i = 0; i < BLINK_CYCLES; i++)
+            {
+                printf("i = %i\n", i);
+                digitalWrite(1, HIGH);
+                delay(300);
+                digitalWrite(1, LOW);
+                delay(300);
+            }
+        }
     }
     return 0;
 }
