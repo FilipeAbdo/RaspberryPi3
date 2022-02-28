06f4d329560f1061987fad196aa21969cfabb31b
FilipeAbdo <filipeibraim@gmail.com>
2022-02-28 20:49:29 -0300

    Adding new files to define stepper controller architecture

---
+++ StepperMotorControl/stepperController.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <wiringPi.h>
+
+
+#include "stepperController.h"
+
---
+++ StepperMotorControl/stepperController.h
@@ -0,0 +1,6 @@
+#ifndef __STEPPER_CONTROLLER_H__
+#define __STEPPER_CONTROLLER_H__
+
+#include "stepperMotorControlConfig.h"
+
+#endif //__STEPPER_CONTROLLER_H__
